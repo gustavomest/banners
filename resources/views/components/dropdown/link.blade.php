7ac66b6525ba2435f940a119a317e9c2f57444b1
Gustavo Oliveira <gustavoooliveiradecarvalho@gmail.com>
2024-10-22 20:20:35 -0400

    todo

---
+++ resources/views/components/dropdown/link.blade.php
@@ -0,0 +1,6 @@
+@php
+    $classes = 'block w-full px-4 py-2 text-start text-sm leading-5 hover:bg-gray/10 hover:text-clrPrimary
+        focus:outline-none focus:bg-gray/20 focus:text-clrSecondary transition duration-150 ease-in-out';
+@endphp
+
+<a {{ $attributes->merge(['class' => $classes]) }}>{{ $slot }}</a>
